b85e06aa0452b9390cc3ae104a524e6d9b985584
sarit-chaet-hudis <sarit.chh@gmail.com>
2022-01-10 12:52:31 +0200

    users setup

---
+++ Users.cjs
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
